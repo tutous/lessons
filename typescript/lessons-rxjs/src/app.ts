d1704ea383b089b97d31471cafb62566d47fff10
Uwe Sluga <usluga@Uwes-MBP.fritz.box>
2019-09-02 21:28:37 +0200

    lessons-rxjs added

---
+++ typescript/lessons-rxjs/src/app.ts
@@ -0,0 +1,3 @@
+import * as mod_exc01 from "./operators/example01";
+
+mod_exc01;
---
+++ typescript/lessons-rxjs/src/operators/example01.ts
@@ -0,0 +1,38 @@
+export {};
+import { of, from } from "rxjs";
+import { map, filter, first, flatMap } from "rxjs/operators";
+
+of(1, 2, 3, 4)
+  .pipe(
+    map(x => x + 1),
+    filter(x => x > 3),
+    first(x => x == 4)
+  )
+  .subscribe(v => console.log(`value: ${v}`));
+
+from([1, 2, 3, 4])
+  .pipe(
+    map(x => x + 1),
+    filter(x => x > 3),
+    first(x => x == 4)
+  )
+  .subscribe(v => console.log(`value: ${v}`));
+
+let firstX: number;
+from([1, 2, 3, 4])
+  .pipe(
+    map(x => x + 1),
+    filter(x => x > 3),
+    first(x => x == 4)
+  )
+  .subscribe(v => (firstX = v));
+console.log(firstX);
+
+of([1, 2, 3, 4], [5, 6, 7, 8])
+  .pipe(
+    flatMap(x => x),
+    filter(x => x > 3 && x < 6),
+    first(x => x == 4)
+  )
+  .subscribe(v => (firstX = v));
+console.log(firstX);
